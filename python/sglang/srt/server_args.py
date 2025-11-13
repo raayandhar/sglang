e1ca10211a3efdc913e9306a81d9f7db4d169b05
Raayan Dhar raayan.dhar@gmail.com <raayan.dhar@gmail.com>
2025-11-12 16:48:51 -0800

    fix server_args
    Signed-off-by: Raayan Dhar raayan.dhar@gmail.com <raayan.dhar@gmail.com>

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -22,11 +22,13 @@ import logging
 import os
 import random
 import tempfile
-from typing import TYPE_CHECKING, Dict, List, Literal, Optional, Union
+from typing import Dict, List, Literal, Optional, Union
 
 import orjson
 
 from sglang.srt.environ import ToolStrictLevel, envs
+from sglang.srt.function_call.function_call_parser import FunctionCallParser
+from sglang.srt.lora.lora_registry import LoRARef
 from sglang.srt.parser.reasoning_parser import ReasoningParser
 from sglang.srt.utils.common import (
     LORA_TARGET_ALL_MODULES,
@@ -59,10 +61,6 @@ from sglang.srt.utils.common import (
     xpu_has_xmx_support,
 )
 from sglang.utils import is_in_ci
-
-if TYPE_CHECKING:
-    from sglang.srt.function_call.function_call_parser import FunctionCallParser
-    from sglang.srt.lora.lora_registry import LoRARef
 
 logger = logging.getLogger(__name__)
 
@@ -3841,8 +3839,6 @@ class ServerArgs:
         ), "schedule_conservativeness must be non-negative"
 
     def check_lora_server_args(self):
-        from sglang.srt.utils.lora_utils import LoRARef
-
         assert self.max_loras_per_batch > 0, "max_loras_per_batch must be positive"
 
         # Enable LoRA if any LoRA paths are provided for backward compatibility.
